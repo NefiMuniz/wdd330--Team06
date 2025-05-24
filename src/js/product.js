e817f3b35456a3675ec1671391765447c26c6fb0
rysmartinez <rysmartinez7@gmail.com>
2025-05-23 20:01:40 -0500

    W03 Individual Task(s): Report

--- src/js/product.js
+++ src/js/product.js
@@ -2,6 +2,8 @@ import { getLocalStorage, getParam, setLocalStorage } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
 import ProductDetails from "./ProductDetails.mjs";
 
+/*loadHeaderFooter();*/
+
 const dataSource = new ProductData("tents");
 const productId = getParam("product");
 
