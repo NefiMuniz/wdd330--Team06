04d52c0bca51fe636c7fa7bf2095b2e7db081b27
Tiago Neves <tiagorockman@gmail.com>
2025-05-27 21:17:25 -0300

    Product search added

--- src/js/product-listing.js
+++ src/js/product-listing.js
@@ -10,3 +10,23 @@ const element = document.querySelector(".product-list");
 const listing = new ProductList(category, dataSource, element);
 
 listing.init();
+
+
+// Search functionality
+const searchButton = document.getElementById("search-button");
+const searchInput = document.getElementById("search-input");
+
+//function to handle search
+const handleSearch = () => {
+     const query = searchInput.value.toLowerCase();
+    listing.filterProducts(query);
+    searchInput.value = "";
+}
+
+searchButton.addEventListener("click", handleSearch);
+
+//On Enter Key
+searchInput.addEventListener("keydown", (event) =>{
+    if(event.key === "Enter")
+        handleSearch();
+})
