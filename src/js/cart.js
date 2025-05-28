7e5fbe568a31f2c18e1040a50ca936d8219b89a5
nefims <nefims@gmail.com>
2025-05-28 19:31:52 -0300

    Fixing cart images

--- src/js/cart.js
+++ src/js/cart.js
@@ -8,7 +8,7 @@ function cartItemTemplate(item) {
   return `
     <li class="cart-card divider">
       <a href="#" class="cart-card__image">
-        <img src="${item.Image}" alt="${item.Name}" />
+        <img src="${item.Images?.PrimarySmall || "default.jpg"}" alt="${item.Name}" />
       </a>
       <a href="#">
         <h2 class="card__name">${item.Name}</h2>
